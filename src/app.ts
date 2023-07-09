e15828f22534df6a9e24b30b2c0e3d2e149c194b
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-07-09 18:06:02 -0300

    feat: index-router.ts created

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,26 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express, { Express } from 'express';
 import cors from 'cors';
+import router from '@/routers/index-router';
 
 import { loadEnv, connectDb, disconnectDB } from '@/config';
 
 loadEnv();
 
 import { handleApplicationErrors } from '@/middlewares';
-import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter, ticketsRouter} from '@/routers';
+//import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter, ticketsRouter} from '@/routers';
 
 const app = express();
 app
   .use(cors())
   .use(express.json())
   .get('/health', (_req, res) => res.send('OK!'))
-  .use('/users', usersRouter)
+  /*.use('/users', usersRouter)
   .use('/auth', authenticationRouter)
   .use('/event', eventsRouter)
   .use('/enrollments', enrollmentsRouter)
-  .use(ticketsRouter)
+  .use(ticketsRouter) */
+  .use(router)
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
